7393afa9718ac661fd2cec7d71eda611e0c8691c
Prizrak9 <pavelgolotuk@gmail.com>
2024-12-03 20:04:07 +0200

    Add project files.

---
+++ NewVersion/Program.cs
@@ -0,0 +1,57 @@
+﻿using ImageMagick.Drawing;
+using ImageMagick;
+using System.IO;
+
+namespace NewVersion;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var settings = new MagickReadSettings()
+        {
+            Width = 15,
+            Height = 15,
+        };
+        using var img = new MagickImage("xc:#ff0080", settings);
+        var drawables = new Drawables();
+        drawables
+            .DisableStrokeAntialias()
+            .StrokeColor(MagickColors.White)
+            .FillColor(MagickColors.White)
+            .Polygon([new PointD(5, 5), new PointD(5, 10), new PointD(10, 10), new PointD(10, 5)]);
+
+        img.Draw(drawables);
+
+        string[] colormap = [
+            "#00FF00",
+            "#FF0000",
+            "#FFFC00",
+            "#00E1FF",
+            "#00C8FF",
+            "#0096FF",
+            "#0064FF",
+            "#0000FF",
+            "#0000E1",
+            "#0000C8",
+            "#000096",
+            "#000064",
+            "#005500",
+            "#007D00",
+            "#009E00",
+            "#18CE00",
+            "#FF0080",
+            "#FFFFFF",
+        ];
+
+        var qs = new QuantizeSettings
+        {
+            Colors = 256,
+            DitherMethod = DitherMethod.No,
+        };
+        img.ColorType = ColorType.Palette;
+        img.ColorSpace = ColorSpace.sRGB;
+        img.Remap(colormap.Select(n => new MagickColor(n)), qs);
+        img.WriteAsync("img.png");
+    }
+}
---
+++ OldVersion/Program.cs
@@ -0,0 +1,50 @@
+﻿using ImageMagick;
+
+namespace OldVersion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var settings = new MagickReadSettings()
+            {
+                Width = 15,
+                Height = 15,
+            };
+            using var img = new MagickImage("xc:#ff0080", settings);
+            var drawables = new Drawables();
+            drawables
+                .DisableStrokeAntialias()
+                .StrokeColor(MagickColors.White)
+                .FillColor(MagickColors.White)
+                .Polygon([new PointD(5, 5), new PointD(5, 10), new PointD(10, 10), new PointD(10, 5)]);
+
+            img.Draw(drawables);
+
+            img.Settings.SetDefine("png:color-type", "1");
+            string[] colormap = [
+                "#00FF00",
+                "#FF0000",
+                "#FFFC00",
+                "#00E1FF",
+                "#00C8FF",
+                "#0096FF",
+                "#0064FF",
+                "#0000FF",
+                "#0000E1",
+                "#0000C8",
+                "#000096",
+                "#000064",
+                "#005500",
+                "#007D00",
+                "#009E00",
+                "#18CE00",
+                "#FF0080",
+                "#FFFFFF",
+            ];
+
+            img.Map(colormap.Select(n => new MagickColor(n)));
+            img.Write("img.png");
+        }
+    }
+}
